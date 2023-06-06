6f39cb18dc8c8306a519c528eb4c55fd4edf545e
EC2 Default User <ec2-user@ip-172-31-68-27.ec2.internal>
2023-06-06 14:24:04 +0000

    commit done

--- attributes.tf
+++ attributes.tf
@@ -4,7 +4,7 @@ resource "aws_eip" "lb" {
 }
 
 output "eip" {
-  value = aws_eip.lb
+  value = aws_eip.lb.public_ip
 }
 
 resource "aws_s3_bucket" "mys3" {
@@ -12,5 +12,5 @@ resource "aws_s3_bucket" "mys3" {
 }
 
 output "mys3bucket" {
-  value = aws_s3_bucket.mys3
+  value = aws_s3_bucket.mys3.bucket_domain_name
 }
